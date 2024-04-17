0c81cd9cc1dc0fe8ef4b84fcbbd42a6d91a44918
nilu19h <163760793+nilu19h@users.noreply.github.com>
2024-04-17 11:53:13 +0530

    Update geo.cpp

--- geo.cpp
+++ geo.cpp
@@ -5,6 +5,12 @@ struct P {
     int x, y;
 };
 
+double dist(int x1, int y1, int x2, int y2) {
+    int dx = abs(x1 - x2);
+    int dy = abs(y1 - y2);
+    return sqrt((dx * dx) + (dy * dy));
+}
+
 int main() {
     int n;
     cin >> n;
@@ -12,4 +18,6 @@ int main() {
     for(int i = 0; i < n; ++i) {
         cin >> p[i].x >> p[i].y;
     }
+
+    
 }
