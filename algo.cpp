6de343a02961dc1ae1036d2d38593f10108a7397
nilu19h <163760793+nilu19h@users.noreply.github.com>
2024-04-17 23:33:27 +0530

    Update algo.cpp

--- algo.cpp
+++ algo.cpp
@@ -1,27 +1,4 @@
-#include <iostream>
-#include <vector>
-using namespace std;
+/*
+hsjsjdjd
 
-int n = 3;
-
-void rec(int k) {
-    vector<int> subset;
-    if(k == n) {
-        // process subset
-        for(int x : subset) {
-            cout << x << " ";
-        }
-        cout << endl;
-    }
-    else {
-        rec(k + 1);
-        subset.push_back(k);
-        rec(k + 1);
-        subset.pop_back();
-    }
-}
-
-int main() {
-    // cin >> n;
-    rec(0);
-}
+*/
