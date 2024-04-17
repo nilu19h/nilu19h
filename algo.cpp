052b7826bef280485a929b3336cd48cc2686c8e4
nilu19h <163760793+nilu19h@users.noreply.github.com>
2024-04-17 23:38:34 +0530

    Update algo.cpp

--- algo.cpp
+++ algo.cpp
@@ -1,4 +1,15 @@
 /*
-hsjsjdjd
+  Graph traversal
 
+  - depth-first search (DFS)
+  - breadth-first search (BFS)
+
+  both algorithms are given a starting node.
+  they visit all nodes that can be reached
+  from them. the difference in the algorithms
+  is the order in which they visit the nodes
+
+  - Depth-First Search
+    
+    
 */
